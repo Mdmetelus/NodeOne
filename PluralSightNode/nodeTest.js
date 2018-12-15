eaccfab554b564ea886cf2fa9fa1068933673bc6
Mr. Metelus <Mdmetelus@gmail.com>
2018-12-14 20:00:06 -0500

    "added set timeout functions"

--- PluralSightNode/nodeTest.js
+++ PluralSightNode/nodeTest.js
@@ -3,3 +3,20 @@ function today() {
     return new Date();
    
  }
+
+
+ //set time out example printssthe greeting after 4 seconds.
+ setTimeout(() => {
+     console.log('Hello after 4 seconds (1)');
+ }, 4*1000);
+
+ setTimeout(() => {
+    console.log('Hello after 4 seconds(2)');
+}, 4000);
+
+
+const func = () => {
+    console.log('Hello after 4 seconds(3)');
+};
+
+setTimeout(func,4000);
