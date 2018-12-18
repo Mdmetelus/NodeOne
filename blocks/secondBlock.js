b502fda24bb4bfd953eb22dc897cc8452216e441
Mr. Metelus <Mdmetelus@gmail.com>
2018-12-18 15:55:41 -0500

    "bc constructor class created"

--- blocks/secondBlock.js
+++ blocks/secondBlock.js
@@ -14,3 +14,10 @@ class Block {
 // const fooBlock = new Block('foo-data','foo-hash', 'foo-lastHash');
 // console.log(fooBlock);
 
+class Blockchain {
+    constructor() {
+        const genesis = new Block('gen-data', 'gen-hash'," gen-lastHash")
+        this.chain = [genesis];
+    }
+
+}
