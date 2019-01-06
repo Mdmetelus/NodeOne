da268ee2fa290c0b6a1c8fe2978a6d5e22b2de4b
Mr. Metelus <Mdmetelus@gmail.com>
2019-01-06 16:30:30 -0500

    "super basic crud implimented"

--- Pract/hello-world-express/server.js
+++ Pract/hello-world-express/server.js
@@ -20,15 +20,35 @@ server.get('/', (req, res) => {
 // const serveer and the server.get are the express way of doing the const server  using node.js, it changes to an express server from an http server.
 // server.get is the  READ DATA one
 server.get('/hobbits', (req,res) => {
+
+
     res.status(200).send('Welcome to Hobbiton');
 });
+
 
 
 //status code 201 belongs to only  post, and creating new date successfully.
 // server.post is the CREATE DATA one
 server.post(`/hobbits`, (req, res) => {
-    res.status(201).json();
+
+
+    res.status(201).json({ url: '/hobbits', operation: 'POST'});
 });
+
+
+
+//server.put is the UPDATE DATA option.
+server.put('hobbits', (req,res) => {
+
+
+    res.status(200).json({ url: '/hobbits', operation: 'PUT'})
+}); // Put refers to update, so any time we want to change the data we use it.
+
+//204 status code lets us know something has been deleted. we don't need any
+// server.delete is the DELETE/DESTROY DATA one.
+server.delete('/hobbits', (req,res) => {
+    res.status(204);
+}); // DELETE/DESTROY DATA 
 
 
 
