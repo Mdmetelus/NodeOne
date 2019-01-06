0970d79a64e5a8e9e7b2d5ff7ab3270d002523ee
Mr. Metelus <Mdmetelus@gmail.com>
2019-01-06 17:29:09 -0500

    "..."

--- Pract/randomRoutes.js
+++ Pract/randomRoutes.js
@@ -8,3 +8,24 @@ server.get('/about', (req,res) => {
 server.get('/contact', ( req, res) => {
     res.status(200).send(`<h1>Contact Form</h1>`);
 });
+
+server.delete('/hobbits/:id', (req,res) => {
+    //`:id` will represent whatever comes after hobbits/
+});
+
+server.delete(`hobbits/:id`, (req, res) => {
+    const id = req.params.id;
+    //or we could destructure it like this const { id } = req.params;
+    res.status(200).json({
+    url: `/hobbits/${id}`,
+        operation: `DELETE for hobbits with id ${id}`,
+    });
+});
+
+//example:
+// locolhost:500/hobbits/12 will be represented by the following in postman:
+//{
+//     "url": "/hobbits/12",
+//     "operation": "DELETE for hobbits with id 12"
+// }
+
