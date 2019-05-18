e47375b23d392712b6b1db4cc26c4b3c9c3cad37
Mr. Metelus <Mdmetelus@gmail.com>
2019-05-18 16:57:05 -0400

    js practice

--- tutes/Random1/play.js
+++ tutes/Random1/play.js
@@ -1,11 +1,20 @@
-var myName = 'Max';
+const myName = 'Max';
 console.log ('Hello ' + myName +'!');
-var age = 35;
-var hasHobbies = true;
+const age = 35;
+const hasHobbies = true;
 
-function summarizeUser(userName, userAge, userHasHobby) {
+const summarizeUser = function summarizeUser(userName, userAge, userHasHobby) {
     return (
         'Name is ' + userName + ', age is ' + userAge + ' and the user has hobbies: ' + userHasHobby
     );
 }
 console.log(summarizeUser(myName, age, hasHobbies));
+
+const add = (a,b) => a + b;
+console.log(add(1,7));
+
+const addOne = a => a + 1;
+console.log(addOne(5));
+
+const addRandom = () => 1 + 2;
+console.log(addRandom());
