29a7fc0131f886a1d4d0f1b987728d3a37acd6d7
Mr. Metelus <Mdmetelus@gmail.com>
2019-05-21 02:22:44 -0400

    req and res

--- tutes/Server/app.js
+++ tutes/Server/app.js
@@ -2,7 +2,11 @@ const http = require('http');
 
 
 const server = http.createServer((req,res) => {
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
+    // console.log(req);
+    // process.exit(); allows you to exit the event loop.
+    res.setHeader('Content-Type', 'text/html');
+    res.write()
 });
 
 server.listen(3111);
