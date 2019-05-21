23167287a1aa3b302a30ad66e55581e83dc6721f
Mr. Metelus <Mdmetelus@gmail.com>
2019-05-21 16:12:10 -0400

    middleware tests and manipulations

--- tutes/Server2/app.js
+++ tutes/Server2/app.js
@@ -1,20 +1,27 @@
-const http = require('http');
+// const http = require('http');
 
 const express = require('express');
 
 const app = express();
 
-app.use((req,res, next) => {
-    console.log('In the Middleware!');
+app.use('/',(req, res, next) => {
+    console.log('This always Runs!');
     next();
 });
 
-app.use((req,res, next) => {
-    console.log('In the Middleware!');
+app.use('/add-product', (req, res, next) => {
+    console.log('In another Middleware!');
+    res.send('<hi>The "Add Product" Page</h1>');
+   
+});
+app.use('/', (req, res, next) => {
+    console.log('In another Middleware!');
     // ...
-    next();
+    res.send('<hi>Hello form Express!</h1>');
+    // next();
 });
 
-const server = http.createServer(app);
-
-server.listen(3535);
+// const server = http.createServer(app);
+// server.listen(3535);
+// alternatively we can just 
+app.listen(3535);
