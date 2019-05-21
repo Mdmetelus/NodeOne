42891905585f9b23be117fbe9ae3b5189af2dacb
Mr. Metelus <Mdmetelus@gmail.com>
2019-05-21 18:01:07 -0400

    middleware, get,  and post added also bodyParser

--- tutes/Server2/app.js
+++ tutes/Server2/app.js
@@ -1,19 +1,44 @@
 // const http = require('http');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded());// parses bpdes sent through forms
+
+
+
+app.use('/add-product1', (req, res, next) => {
+    console.log('In another Middleware!');
+    res.send('<html><body><form action="/product" method="POST"><input type="text" name="title"><button type="submit"> Add Product 1 </button></form></body></html>'); 
+    
+});
+
+app.use('/add-product', (req, res, next) => {
+    console.log('In another Middleware!');
+    res.send('<hi>The "Add Product" Page</h1>');
+    
+});
+
+app.get('/product', (req, res, next) => {
+    console.log(req.body);
+    
+    res.redirect('/');
+});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    
+    res.redirect('/');
+});
 
 app.use('/',(req, res, next) => {
     console.log('This always Runs!');
     next();
 });
 
-app.use('/add-product', (req, res, next) => {
-    console.log('In another Middleware!');
-    res.send('<hi>The "Add Product" Page</h1>');
-   
-});
+
 app.use('/', (req, res, next) => {
     console.log('In another Middleware!');
     // ...
