b790a9ac6e07ae2bd540f52d7957bb9daaa5405c
Mr. Metelus <Mdmetelus@gmail.com>
2019-05-21 00:51:02 -0400

    asyci and nested callback/async completed

--- tutes/Random1/async.js
+++ tutes/Random1/async.js
@@ -1,6 +1,18 @@
+// import { callbackify } from "util";
+
+const fetchData = callback => {
+    setTimeout(() => {
+        callback('Done! I\'m done this nested Callback');
+    }, 1500);
+}
+
+
 setTimeout(() => {
-    console.log('Timer is done! \n This is async code.');
+    console.log('Timer is done! \n This is async code.\n');
+    fetchData((text) => {
+        console.log(text);
+    });
 }, 2001);
 
 console.log('Hello');
-console.log('H1!');
+console.log('H1!\n');
