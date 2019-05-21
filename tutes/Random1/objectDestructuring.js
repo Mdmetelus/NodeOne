abccd9b244b37c19b6f8df16ffc585060cd15d66
Mr. Metelus <Mdmetelus@gmail.com>
2019-05-20 21:23:55 -0400

    start

---
+++ tutes/Random1/objectDestructuring.js
@@ -0,0 +1,10 @@
+const person = {
+    name: 'Max',
+    age: 35,
+    greet() {
+        console.log('Hi, I am ' + this.name);
+    }
+    // goodBye: function() {
+        // console.log('Goodbye ' + this.name + '!');
+    // }
+};
