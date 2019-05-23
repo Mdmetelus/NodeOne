730e8a041d52e3cedd21dccd2757e2c4788ad65f
Mr. Metelus <Mdmetelus@gmail.com>
2019-05-22 22:30:37 -0400

    app adjustments

--- tutes/Server2/app.js
+++ tutes/Server2/app.js
@@ -5,37 +5,43 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));// parses bpdes sent through forms
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
 
-app.use('/add-product', (req, res, next) => {
-    console.log('In another Middleware!');
-    res.send('<hi>The "Add Product" Page</h1>');
+// app.use('/add-product', (req, res, next) => {
+//     console.log('In another Middleware!');
+//     res.send('<hi>The "Add Product" Page</h1>');
     
-});
+// });
 
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
+// app.post('/product', (req, res, next) => {
+//     console.log(req.body);
     
-    res.redirect('/');
-});
+//     res.redirect('/');
+// });
 
-app.use('/',(req, res, next) => {
-    console.log('This always Runs!');
-    next();
-});
+// app.use('/',(req, res, next) => {
+//     console.log('This always Runs!');
+//     next();
+// });
 
 
-app.use('/', (req, res, next) => {
-    console.log('In another Middleware!');
-    // ...
-    res.send('<hi>Hello form Express!</h1>');
-    // next();
-});
+// app.use('/', (req, res, next) => {
+//     console.log('In another Middleware!');
+//     // ...
+//     res.send('<hi>Hello form Express!</h1>');
+//     // next();
+// });
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found.</h1>')
+})
 
 // const server = http.createServer(app);
 // server.listen(3535);
