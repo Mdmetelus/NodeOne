78f79efd9a6a98c1854573b0b0e56cc362961c70
Mr. Metelus <Mdmetelus@gmail.com>
2019-05-22 22:31:41 -0400

    shop route file and route

--- tutes/Server2/routes/shop.js
+++ tutes/Server2/routes/shop.js
@@ -1,12 +1,18 @@
+const path = require('path');
 const express = require('express');
 
 const router = express.Router();
 
+// router.get('/', (req, res, next) => {
+//     // console.log('In another Middleware!');
+//     // ...
+//     res.send('<hi>Hello form Express!</h1>');
+//     // next();
+// });
 router.get('/', (req, res, next) => {
-    // console.log('In another Middleware!');
     // ...
-    res.send('<hi>Hello form Express!</h1>');
-    // next();
+    res.sendFile(path.join(__dirname,'../','views', 'shop.html'));
+    
 });
 
 
