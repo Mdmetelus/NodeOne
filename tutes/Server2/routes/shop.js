ebbdc3dbc24f45a7dc05dc4b0440d806d9bbed5b
Mr. Metelus <Mdmetelus@gmail.com>
2019-05-22 22:32:35 -0400

    update

--- tutes/Server2/routes/shop.js
+++ tutes/Server2/routes/shop.js
@@ -11,7 +11,7 @@ const router = express.Router();
 // });
 router.get('/', (req, res, next) => {
     // ...
-    res.sendFile(path.join(__dirname,'../','views', 'shop.html'));
+    res.sendFile(path.join(__dirname, '../','views', 'shop.html'));
     
 });
 
